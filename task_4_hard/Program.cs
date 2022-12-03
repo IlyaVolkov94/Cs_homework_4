da2806cf890d73941affe08fe99689bf3a1b827e
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2022-12-03 16:36:00 +0300

    task_5_complete

--- task_4_hard/Program.cs
+++ task_4_hard/Program.cs
@@ -1,10 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Напишите программу, которая принимает на вход целое или дробное число и выдаёт количество цифр в числе.
 
 try
 {
-Console.WriteLine("Введите число");
-double Number = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Количество цифр в числе: "+CheckSumIndex(Number));
+    Console.WriteLine("Введите число");
+    double Number = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Количество цифр в числе: " + CheckSumIndex(Number));
 }
 catch
 {
@@ -14,25 +14,25 @@ catch
 //Функция подсчета разрядов в числе
 int CheckSumIndex(double Num)
 {
-double NumFract = Num;
-int CountMod=0;
-if(Num<0) Num=-Num;
-if(Num<1)   //Считает разряды числе <1
-{
-    CountMod++;
-    goto Fract;
-}
-while(Num>=1)
-{
-    Num/=10;
-    CountMod++;
-}
+    double NumFract = Num;
+    int CountMod = 0;
+    if (Num < 0) Num = -Num;
+    if (Num < 1)   //Считает разряды числе <1
+    {
+        CountMod++;
+        goto Fract;
+    }
+    while (Num >= 1)
+    {
+        Num /= 10;
+        CountMod++;
+    }
 Fract:
-int CountFract = -1;
-while(NumFract%10!=0)
-{
-    NumFract*=10;
-    CountFract++;
-}
-return CountMod+CountFract;
+    int CountFract = -1;
+    while (NumFract % 10 != 0)
+    {
+        NumFract *= 10;
+        CountFract++;
+    }
+    return CountMod + CountFract;
 }
