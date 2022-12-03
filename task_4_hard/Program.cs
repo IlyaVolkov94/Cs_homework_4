57222ac67f320248b18b9d8a855cc2d847b9d2a1
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2022-12-03 15:10:14 +0300

    task_4_up

--- task_4_hard/Program.cs
+++ task_4_hard/Program.cs
@@ -15,17 +15,24 @@ catch
 int CheckSumIndex(double Num)
 {
 double NumFract = Num;
+int CountMod=0;
+if(Num<0) Num=-Num;
+if(Num<1)
+{
+    CountMod++;
+    goto Fract;
+}
+while(Num>=1)
+{
+    Num/=10;
+    CountMod++;
+}
+Fract:
 int CountFract = -1;
 while(NumFract%10!=0)
 {
     NumFract*=10;
     CountFract++;
-}
-int CountMod=0;
-while(Num>=1)
-{
-    Num/=10;
-    CountMod++;
 }
 return CountMod+CountFract;
 }
