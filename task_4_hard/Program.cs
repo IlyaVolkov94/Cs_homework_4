03e8c1aaa38f3f7e48f0bd2281b6ab3f69fa8705
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2022-12-03 14:45:38 +0300

    task_4

---
+++ task_4_hard/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+try
+{
+Console.WriteLine("Введите число");
+double Number = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Количество цифр в числе: "+CheckSumIndex(Number));
+}
+catch
+{
+    Console.WriteLine("Ошибка выполнения");
+}
+
+//Функция подсчета количества цифр в числе
+int CheckSumIndex(double Num)
+{
+double NumFract = Num;
+int CountFract = 0;
+while(NumFract%10!=0)
+{
+    NumFract*=10;
+    CountFract++;
+}
+CountFract--;
+int CountMod=0;
+while(Num>=1)
+{
+    Num/=10;
+    CountMod++;
+}
+return CountMod+CountFract;
+}
