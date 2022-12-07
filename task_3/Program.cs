15e68ac0eb2cfcdd061f150f804d19de0b88cbe1
Ilya_Volkov <ilyasergeevichvolkov@gmail.com>
2022-12-07 18:42:41 +0300

    complete

---
+++ task_3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов с клавиатуры и выводит массив на экран.
+
+
+int[] InputArray(int Size)  //Функция ввода массива
+{
+    int[] Array = new int[Size];
+    for (int i = 0; i < Size; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент");
+        Array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return Array;
+}
+
+void PrintArray(int[] Array) //Функция для печати массива
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write(Array[i] + " ");
+    }
+    Console.WriteLine("");
+}
+
+try
+{
+    int Size = 8;
+    int[] Array = new int[Size];
+    Array = InputArray(Size);
+    PrintArray(Array);
+}
+
+catch
+{
+    Console.WriteLine("Ошибка выполнения");
+}
